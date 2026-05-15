112efff3d3191d10ebd80e692483cc12ffaa0bf8
Bart Oostveen <bart@bartoostveen.nl>
2026-05-15 22:03:41 +0200

    chore: hotfix for deployments

--- parts/deployments.nix
+++ parts/deployments.nix
@@ -1,12 +1,19 @@
 {
+  # TODO: wireguard dns
   deployments = {
     nixos = {
       bart-server = {
         sshUser = "root";
         ip = "bartoostveen.nl";
       };
-      bart-laptop-new.sshUser = "bart";
-      bart-pc.sshUser = "bart";
+      bart-laptop-new = {
+        sshUser = "bart";
+        ip = "10.0.0.2"; # TODO: remove
+      };
+      bart-pc = {
+        sshUser = "bart";
+        ip = "10.0.0.7"; # TODO: remove
+      };
       atlas = {
         sshUser = "root";
         system = "aarch64-linux";
