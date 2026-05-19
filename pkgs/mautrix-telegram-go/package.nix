6bee3c671e9809a9fdd6d2ea89e8049882406be1
Bart Oostveen <bart@bartoostveen.nl>
2026-05-20 00:04:47 +0200

    mautrix-telegram-go: 0-unstable-2026-04-02 -> 0.2605.0

--- pkgs/mautrix-telegram-go/package.nix
+++ pkgs/mautrix-telegram-go/package.nix
@@ -9,21 +9,20 @@
 
 buildGoModule (finalAttrs: {
   pname = "mautrix-telegram";
-  version = "0-unstable-2026-04-02";
+  version = "0.2605.0";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "telegram";
-    rev = "e7099d26f39922c32a6d57890860abb573b1cb0c";
-    hash = "sha256-CZdMNcvcZtx01cTVnymECMVINDdD2kqwCpcXeHf3pcU=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-9TCXyGvFCZAv8xIUW3oiVRv5EBdObrLuALfME/oAWBE=";
   };
 
-  vendorHash = "sha256-/fa/FDajT+wnJFP/QNw0NpA4IRWr1f09rDYqwaRG+es=";
+  vendorHash = "sha256-xcBbBIsFXQ90WyQ8OY+CCVIiBepIlOD/o+ZjabNvM0Q=";
 
   ldflags = [
     "-X"
-    # "main.Tag=${finalAttrs.version}"
-    "main.Tag=v0.2604.2"
+    "main.Tag=v${finalAttrs.version}"
   ];
 
   buildInputs = (lib.optional (!withGoOlm) olm) ++ [ stdenv.cc.cc.lib ];
@@ -38,7 +37,7 @@ buildGoModule (finalAttrs: {
     homepage = "https://github.com/mautrix/telegram";
     changelog = "https://github.com/mautrix/telegram/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.agpl3Only;
-    maintainers = with lib.maintainers; [ ];
+    maintainers = with lib.maintainers; [ bartoostveen ];
     mainProgram = "mautrix-telegram";
   };
 })
