1ee4c8cc42713cfd8deb4c15da664fa8dfe571c9
Bart Oostveen <bart@bartoostveen.nl>
2026-05-19 23:23:49 +0200

    github-readme-stats: 2025-12-21 -> 2026-05-19

--- pkgs/github-readme-stats/package.nix
+++ pkgs/github-readme-stats/package.nix
@@ -2,34 +2,26 @@
   lib,
   buildNpmPackage,
   fetchFromGitHub,
-  importNpmLock,
   nodejs_24,
-  applyPatches,
 }:
 
 buildNpmPackage (finalAttrs: {
   pname = "github-readme-stats";
-  version = "0-unstable-2025-12-21";
+  version = "0-unstable-2026-05-19";
 
   src = fetchFromGitHub {
-    owner = "anuraghazra";
+    owner = "bartoostveen";
     repo = "github-readme-stats";
-    rev = "8108ba1417faaad7182399e01dd724777adc63e5";
-    hash = "sha256-DV9Gx6R9qn0ZST+U8VwHeYJJApuifriY2i8r9eunXn0=";
+    rev = "4072cfc0bb99ceed46814b05818138c01c8e8539";
+    hash = "sha256-bOzOI3YSIqgQXahoXW65A5VL+29qmQ388VAuOqh3RJk=";
   };
+
+  npmDepsHash = "sha256-oiB+OA6a/okbWezOODY8EpWPxy6BgnceoXQrOOIZUy4=";
 
   patches = [
     # as specified in https://github.com/anuraghazra/github-readme-stats?tab=readme-ov-file#on-other-platforms
-    ./0001-fix-express-dependency.patch
+    # ./0001-fix-express-dependency.patch
   ];
-
-  npmDeps = importNpmLock {
-    npmRoot = applyPatches {
-      inherit (finalAttrs) src patches;
-    };
-    packageLock = builtins.fromJSON (builtins.readFile ./package-lock.json);
-  };
-  npmConfigHook = importNpmLock.npmConfigHook;
 
   dontNpmBuild = true;
 
