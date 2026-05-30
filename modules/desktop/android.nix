cf7f5f4b9747c685b176f6fcbf16e1d84e605033
Bart Oostveen <bart@bartoostveen.nl>
2026-05-30 23:10:59 +0200

    chore(android): add 16k page size images

--- modules/desktop/android.nix
+++ modules/desktop/android.nix
@@ -7,9 +7,12 @@ let
       "36.0.0"
       "35.0.0"
     ];
-    platformVersions = [ "37" ];
+    platformVersions = [ "37.0" ];
     includeSystemImages = true;
-    systemImageTypes = [ "google_apis_playstore" ];
+    systemImageTypes = [
+      "google_apis_playstore"
+      "google_apis_playstore_ps16k"
+    ];
     includeEmulator = true;
     includeSources = true;
   };
