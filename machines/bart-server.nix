5139370eedad1e2a4a7585107aa02344596edeef
Bart Oostveen <bart@bartoostveen.nl>
2026-06-09 16:33:07 +0200

    chore: add tascheduling db

--- machines/bart-server.nix
+++ machines/bart-server.nix
@@ -109,5 +109,22 @@ in
     ];
   };
 
+  virtualisation.oci-containers.containers.tascheduling-db = {
+    image = "postgres:latest";
+    environment = {
+      POSTGRES_USER = "tascheduling";
+      POSTGRES_DB = "tascheduling";
+    };
+    environmentFiles = [ config.sops.secrets.tascheduling-db-env.path ];
+    volumes = [ "tascheduling-db-data:/var/lib/postgresql" ];
+    ports = [ "5433:5432" ];
+  };
+
+  sops.secrets.tascheduling-db-env = {
+    sopsFile = ../secrets/tascheduling-db.env.bart-server.secret;
+    format = "binary";
+    restartUnits = [ "podman-tascheduling-db.service" ];
+  };
+
   system.stateVersion = "26.11";
 }
