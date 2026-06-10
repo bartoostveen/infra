79e77f5b37a98b7ea4bf454fb9354c910f102d7c
Bart Oostveen <bart@bartoostveen.nl>
2026-06-10 14:49:58 +0200

    feat(ci): gitlab runner

---
+++ modules/gitlab-runner.nix
@@ -0,0 +1,30 @@
+{ config, ... }:
+
+{
+  services.gitlab-runner = {
+    enable = true;
+    services.docker = {
+      authenticationTokenConfigFile = config.sops.secrets.gitlab-runner-env.path;
+      dockerImage = "docker:stable";
+      dockerVolumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
+      tagList = [ "docker" ];
+    };
+  };
+
+  sops.secrets.gitlab-runner-env = {
+    sopsFile = ../secrets/gitlab-runner.env.bart-pc.secret;
+    mode = "0440";
+    owner = "gitlab-runner";
+    group = "gitlab-runner";
+    format = "binary";
+    restartUnits = [ "gitlab-runner.service" ];
+  };
+
+  users.users.gitlab-runner = {
+    isSystemUser = true;
+    group = "gitlab-runner";
+  };
+  users.groups.gitlab-runner = { };
+}
