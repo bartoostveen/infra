023c220f101d6bd55dbe7ee55eb62bde9cdc8847
Bart Oostveen <bart@bartoostveen.nl>
2026-06-16 00:30:25 +0200

    fixup! feat(monitoring): alert if borg backup takes a long time

--- modules/infra/system-specific/main/rules/borg.nix
+++ modules/infra/system-specific/main/rules/borg.nix
@@ -11,12 +11,12 @@
           };
           expr = ''
             (
-              time() - systemd_unit_active_enter_time_seconds{name=~\"borgbackup-.*\\\\.service\"}
+              time() - systemd_unit_active_enter_time_seconds{name=~"borgbackup-.*\\.service"}
             ) * on(name) group_left()
             (
               systemd_unit_state{
-                name=~\"borgbackup-.*\\\\.service\",
-                state=\"active\"
+                name=~"borgbackup-.*\\.service",
+                state="active"
               } == 1
             ) > 7200
           '';
