b482088896021e31aa0ca01ea627725eef71d937
Bart Oostveen <bart@bartoostveen.nl>
2026-06-28 02:50:38 +0200

    chore(home/jetbrains): document x11/wayland issue

--- home/jetbrains.nix
+++ home/jetbrains.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+# HOW TO SWITCH TO X11/XWAYLAND WHEN JETBRAINS FUCKS UP WAYLAND
+# Add `-Dawt.toolkit.name=XToolkit` to idea64.vmoptions (or another IDEs equivalent)
+
 {
   home.packages = with pkgs; [
     jetbrains-toolbox
