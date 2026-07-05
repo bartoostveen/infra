97366933fe0c3138122051f777a5f3629b88b2b1
Bart Oostveen <bart@bartoostveen.nl>
2026-07-06 01:47:41 +0200

    chore(networking): reject lennsteenbergen.com

--- modules/infra/networking.nix
+++ modules/infra/networking.nix
@@ -9,4 +9,8 @@
     "net.ipv4.conf.all.forwarding" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
   };
+
+  networking.firewall.extraCommands = ''
+    iptables -I INPUT -s 77.160.138.6 -j REJECT
+  '';
 }
