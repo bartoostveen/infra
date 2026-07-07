346425c47ede7eaff04fa3e3bbffc9f84e2424d6
Bart Oostveen <bart@bartoostveen.nl>
2026-07-08 00:36:03 +0200

    meshcoredecoder: modernize

--- pkgs/meshcoredecoder/package.nix
+++ pkgs/meshcoredecoder/package.nix
@@ -1,30 +1,36 @@
 {
   lib,
-  python313Packages,
+  python314Packages,
 }:
 
-python313Packages.buildPythonPackage (finalAttrs: {
+let
+  pythonPackages = python314Packages;
+in
+pythonPackages.buildPythonPackage (finalAttrs: {
   pname = "meshcoredecoder";
   version = "0.3.2";
   pyproject = true;
 
-  src = python313Packages.fetchPypi {
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  src = pythonPackages.fetchPypi {
     inherit (finalAttrs) pname version;
     hash = "sha256-QF0FZzU5HZ62maUm1sQtT9QglNVUf94ivBLbXUVEqQQ=";
   };
 
-  build-system = with python313Packages; [
+  build-system = with pythonPackages; [
     setuptools
-    wheel
   ];
 
-  dependencies = with python313Packages; [
+  dependencies = with pythonPackages; [
     click
     cryptography
     pycryptodome
+    wheel
   ];
 
-  optional-dependencies = with python313Packages; {
+  optional-dependencies = with pythonPackages; {
     dev = [
       black
       pylint
@@ -41,5 +47,7 @@ python313Packages.buildPythonPackage (finalAttrs: {
     description = "Complete Python implementation of the MeshCore Packet Decoder";
     homepage = "https://pypi.org/project/meshcoredecoder/";
     license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ bartoostveen ];
+    platforms = lib.platforms.all;
   };
 })
