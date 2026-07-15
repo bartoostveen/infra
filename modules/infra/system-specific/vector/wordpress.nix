e4ced162f4037f39a8aa733c1ad5cb20cc333ce3
Bart Oostveen <bart@bartoostveen.nl>
2026-07-15 19:08:15 +0200

    Revert "chore(customers-vector/wordpress): 7.0.1 -> 7.1-beta1"
    This reverts commit 34516f538ba281fff39b76769b8c20afab9702c3.

--- modules/infra/system-specific/vector/wordpress.nix
+++ modules/infra/system-specific/vector/wordpress.nix
@@ -53,13 +53,7 @@ in
         inherit (pkgs.wordpressPackages.themes) twentytwentyfive;
       };
       languages = [ nl ];
-      package =
-        (pkgs.wordpress_7_0.override { hash = "sha256-2e9AUZF2wpP5NMwP0qjClCdmhrzqwstpPuhL4W6V9TU="; })
-        .overrideAttrs
-          {
-            version = "7.1-beta1";
-            __intentionallyOverridingVersion = true;
-          };
+      package = pkgs.wordpress_7_0;
     };
   };
 
