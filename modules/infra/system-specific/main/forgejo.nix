0d0c6f2bb521cd134077e544ae27d9d70515d61d
Bart Oostveen <bart@bartoostveen.nl>
2026-07-18 00:14:02 +0200

    forgejo: disable flaky tests

--- modules/infra/system-specific/main/forgejo.nix
+++ modules/infra/system-specific/main/forgejo.nix
@@ -25,17 +25,19 @@ in
     enable = true;
     # package = smallPkgs.forgejo;
     # TODO: use upstream again at next nixos-unstable-small
-    package = smallPkgs.callPackage (import "${inputs.nixpkgs}/pkgs/by-name/fo/forgejo/generic.nix" {
-      version = "16.0.0";
-      hash = "sha256-BZawFbrtcxftX4/Yk32aoVRQ6Kg+k1FhN9IoH6dxvVY=";
-      npmDepsHash = "sha256-UhivpUqNJvc3zHxdRVAWT9x68jG1KnQa8yS4KkL2W5g=";
-      vendorHash = "sha256-cb6f7ZX3pG95EEZotGXn6+YUJN59SFNVHFTejFJ6y28=";
-      lts = false;
-      nixUpdateExtraArgs = [
-        "--override-filename"
-        "pkgs/by-name/fo/forgejo/package.nix"
-      ];
-    }) { };
+    package =
+      (smallPkgs.callPackage (import "${inputs.nixpkgs}/pkgs/by-name/fo/forgejo/generic.nix" {
+        version = "16.0.0";
+        hash = "sha256-BZawFbrtcxftX4/Yk32aoVRQ6Kg+k1FhN9IoH6dxvVY=";
+        npmDepsHash = "sha256-UhivpUqNJvc3zHxdRVAWT9x68jG1KnQa8yS4KkL2W5g=";
+        vendorHash = "sha256-cb6f7ZX3pG95EEZotGXn6+YUJN59SFNVHFTejFJ6y28=";
+        lts = false;
+        nixUpdateExtraArgs = [
+          "--override-filename"
+          "pkgs/by-name/fo/forgejo/package.nix"
+        ];
+      }) { }).overrideAttrs
+        { doCheck = false; };
     lfs.enable = true;
     database = {
       createDatabase = true;
