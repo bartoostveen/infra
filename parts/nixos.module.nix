48b91b743464552128c4c0f159c6d11798ff8767
Bart Oostveen <bart@bartoostveen.nl>
2026-07-23 03:17:50 +0200

    flake: expose wireguard metadata

--- parts/nixos.module.nix
+++ parts/nixos.module.nix
@@ -73,56 +73,60 @@ in
   config = {
     _module.args.wireguard = import ../modules/wireguard.meta.nix { inherit lib; };
 
-    flake.nixosConfigurations =
-      (mapAttrs (
-        name:
-        { system, ... }@c:
+    flake = {
+      inherit wireguard;
 
-        let
-          hostname = if c.hostname != null then c.hostname else name;
-        in
-        withSystem system (
-          {
-            pkgs,
-            smallPkgs,
-            continuwuityPkgs,
-            ...
-          }:
+      nixosConfigurations =
+        (mapAttrs (
+          name:
+          { system, ... }@c:
 
-          inputs.nixpkgs.lib.nixosSystem {
-            inherit pkgs system;
+          let
+            hostname = if c.hostname != null then c.hostname else name;
+          in
+          withSystem system (
+            {
+              pkgs,
+              smallPkgs,
+              continuwuityPkgs,
+              ...
+            }:
 
-            specialArgs = {
-              inherit
-                inputs
-                smallPkgs
-                continuwuityPkgs
-                wireguard
-                ;
-            };
+            inputs.nixpkgs.lib.nixosSystem {
+              inherit pkgs system;
 
-            modules = [
-              inputs.sops-nix.nixosModules.sops
-              { networking.hostName = hostname; }
-              ../machines/${hostname}.nix
-            ];
-          }
-        )
-      ) config.deployments.nixos)
-      // mapAttrs (
-        name:
-        { arch, ... }:
-        withSystem arch (
-          {
-            pkgs,
-            ...
-          }:
-          inputs.nixpkgs.lib.nixosSystem {
-            inherit pkgs;
-            specialArgs = { inherit inputs; };
-            modules = [ ../images/${name}.nix ];
-          }
-        )
-      ) config.deployments.extraNixOSConfigurations;
+              specialArgs = {
+                inherit
+                  inputs
+                  smallPkgs
+                  continuwuityPkgs
+                  wireguard
+                  ;
+              };
+
+              modules = [
+                inputs.sops-nix.nixosModules.sops
+                { networking.hostName = hostname; }
+                ../machines/${hostname}.nix
+              ];
+            }
+          )
+        ) config.deployments.nixos)
+        // mapAttrs (
+          name:
+          { arch, ... }:
+          withSystem arch (
+            {
+              pkgs,
+              ...
+            }:
+            inputs.nixpkgs.lib.nixosSystem {
+              inherit pkgs;
+              specialArgs = { inherit inputs; };
+              modules = [ ../images/${name}.nix ];
+            }
+          )
+        ) config.deployments.extraNixOSConfigurations;
+    };
   };
 }
