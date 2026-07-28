26ddfa12c655cc1472c24931ffd94a289758ff9d
Bart Oostveen <bart@bartoostveen.nl>
2026-07-28 16:26:36 +0200

    bart-laptop-new: temporarily disable davinci resolve

--- machines/bart-laptop-new.nix
+++ machines/bart-laptop-new.nix
@@ -106,10 +106,10 @@
     [
       inputs.winapps.packages.${system}.winapps
       inputs.winapps.packages.${system}.winapps-launcher
-      (pkgs.writeShellScriptBin "davinci-resolve" ''
-        ROC_ENABLE_PRE_VEGA=1 RUSTICL_ENABLE=amdgpu,amdgpu-pro,radv,radeon DRI_PRIME=1 QT_QPA_PLATFORM=xcb \
-          ${lib.getExe pkgs.davinci-resolve}
-      '')
+      # (pkgs.writeShellScriptBin "davinci-resolve" ''
+      #   ROC_ENABLE_PRE_VEGA=1 RUSTICL_ENABLE=amdgpu,amdgpu-pro,radv,radeon DRI_PRIME=1 QT_QPA_PLATFORM=xcb \
+      #     ${lib.getExe pkgs.davinci-resolve}
+      # '')
     ];
 
   system.stateVersion = "26.11";
