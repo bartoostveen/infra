24ad5bbfb4c352f87d2b0a0e445b7a513ff4d0fc
Bart Oostveen <bart@bartoostveen.nl>
2026-07-31 02:12:35 +0200

    go.bartoostveen.nl: switch to timedout-registry

--- modules/infra/system-specific/main/gopkgs.nix
+++ modules/infra/system-specific/main/gopkgs.nix
@@ -1,25 +1,74 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
 
 let
   gitDomain = config.services.forgejo.settings.server.DOMAIN;
+  goDomain = "go.bartoostveen.nl";
+  port = 21434;
+
+  inherit (lib)
+    nameValuePair
+    genAttrs'
+    ;
 in
 {
-  services.nginx.virtualHosts."go.bartoostveen.nl" = {
+  imports = [ inputs.bart-packages.nixosModules.timedout-registry ];
+
+  services.timedout-registry = {
+    enable = true;
+    package = pkgs.local.timedout-registry;
+    settings.listen = "127.0.0.1:${toString port}";
+    entries =
+      genAttrs'
+        [
+          # keep-sorted start
+          "bulkmailer"
+          "cpanel-matrix"
+          "invoice"
+          "meowbot"
+          "nix-oci-lock"
+          # keep-sorted end
+        ]
+        (
+          mod:
+          nameValuePair mod {
+            import_path = "${goDomain}/${mod}";
+            repo_url = "https://${gitDomain}/bart/${mod}";
+          }
+        );
+  };
+
+  services.nginx.virtualHosts.${goDomain} = {
     enableACME = true;
     forceSSL = true;
-    locations = {
-      "/".return = "302 https://bartoostveen.nl";
-      "~ ^/([A-Za-z0-9._-]+)/?$".extraConfig = ''
-        if ($arg_go-get = "1") {
-            add_header Content-Type "text/html; charset=utf-8";
-            return 200 "<!doctype html>
-            <html><head>
-            <meta name=\"go-import\" content=\"go.bartoostveen.nl/$1 git https://${gitDomain}/bart/$1\">
-            </head></html>";
-        }
-
-        return 302 https://bartoostveen.nl;
-      '';
-    };
+    locations."/".proxyPass = "http://127.0.0.1:${toString port}";
   };
 }
+
+# {
+#   bulkmailer = {
+#     import_path = "go.bartoostveen.nl/bulkmailer";
+#     repo_url = "https://git.bartoostveen.nl/bart/bulkmailer";
+#   };
+#   cpanel-matrix = {
+#     import_path = "go.bartoostveen.nl/cpanel-matrix";
+#     repo_url = "https://git.bartoostveen.nl/bart/cpanel-matrix";
+#   };
+#   invoice = {
+#     import_path = "go.bartoostveen.nl/invoice";
+#     repo_url = "https://git.bartoostveen.nl/bart/invoice";
+#   };
+#   meowbot = {
+#     import_path = "go.bartoostveen.nl/meowbot";
+#     repo_url = "https://git.bartoostveen.nl/bart/meowbot";
+#   };
+#   nix-oci-lock = {
+#     import_path = "go.bartoostveen.nl/nix-oci-lock";
+#     repo_url = "https://git.bartoostveen.nl/bart/nix-oci-lock";
+#   };
+# }
