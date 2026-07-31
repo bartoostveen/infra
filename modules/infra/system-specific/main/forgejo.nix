5bac3e60b1a440e442175cc742096a210f6d2f91
Bart Oostveen <bart@bartoostveen.nl>
2026-07-31 06:25:57 +0200

    forgejo: use upstream
    This reverts commit eb116e784c2bb917550db815c2c3ae4e61c5f57a.

--- modules/infra/system-specific/main/forgejo.nix
+++ modules/infra/system-specific/main/forgejo.nix
@@ -23,15 +23,7 @@ in
 {
   services.forgejo = {
     enable = true;
-    # package = smallPkgs.forgejo;
-    # TODO: use upstream again at next nixos-unstable-small
-    package = smallPkgs.callPackage (import "${inputs.nixpkgs}/pkgs/by-name/fo/forgejo/generic.nix" {
-      version = "16.0.2";
-      hash = "sha256-cJWX77ROA9I1Y6/IEoGhm6wc2ScgKkMclpEyd4lHqew=";
-      npmDepsHash = "sha256-SS9Yb4fEY9C+ddk2Omf4+v1w6Ff2ArJZd1p5XGRN2EU=";
-      vendorHash = "sha256-iLzYUo+oTSiJbqp7NygfaUxZxeXYuQm1/9oTk5+n0dk=";
-      lts = false;
-    }) { };
+    package = smallPkgs.forgejo;
     lfs.enable = true;
     database = {
       createDatabase = true;
