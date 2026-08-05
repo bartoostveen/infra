f30532ec67ab3b20cb38f31def36f44d03661702
Bart Oostveen <bart@bartoostveen.nl>
2026-08-05 04:19:37 +0200

    meshcoretomqtt: fix evaluation warning

--- modules/infra/system-specific/atlas/mc2mqtt.nix
+++ modules/infra/system-specific/atlas/mc2mqtt.nix
@@ -1,10 +1,14 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
+let
+  inherit (pkgs.stdenv.hostPlatform) system;
+in
 {
   imports = [ inputs.meshcoretomqtt.nixosModules.default ];
 
   services.mctomqtt = {
     enable = true;
+    package = inputs.meshcoretomqtt.packages.${system}.default;
     iata = "ENS";
     serialPorts = [ "/dev/ttyACM0" ];
     defaults = {
