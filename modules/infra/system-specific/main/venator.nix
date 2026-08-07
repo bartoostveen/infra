510e5d7f82484e6e0294468bde37d10801d3e745
Bart Oostveen <bart@bartoostveen.nl>
2026-08-07 05:11:14 +0200

    venator: migrate to bart-packages

--- modules/infra/system-specific/main/venator.nix
+++ modules/infra/system-specific/main/venator.nix
@@ -1,16 +1,25 @@
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+
 let
   port = 8449;
   domain = "venator.omeduostuurcentenneef.nl";
   federationDomain = "server.${domain}";
 in
 {
-  imports = [ ../../venator.nix ];
+  imports = [ inputs.bart-packages.nixosModules.venator ];
 
   services.venator = {
     enable = true;
+    package = pkgs.local.venator;
     configurePostgres = true;
+    enableWrapper = true;
     settings = {
-      registration.enabled = false;
+      registration.admin_pre_shared_secret_file = config.sops.secrets.venator-admin-psk.path;
       server_name = domain;
       listeners = [
         {
@@ -20,6 +29,23 @@ in
       ];
       well_known.client = "https://${federationDomain}";
     };
+  };
+
+  users = {
+    users.venator = {
+      isSystemUser = true;
+      group = "venator";
+    };
+    groups.venator = { };
+  };
+
+  sops.secrets.venator-admin-psk = {
+    sopsFile = ../../../../secrets/venator-admin-psk.bart-server.secret;
+    format = "binary";
+    owner = "venator";
+    group = "venator";
+    mode = "0440";
+    reloadUnits = [ "venator.service" ];
   };
 
   services.nginx.virtualHosts = {
