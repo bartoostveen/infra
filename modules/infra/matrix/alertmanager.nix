403eaaa8af3f51374f3a798c0943934ec6e0c66d
Bart Oostveen <bart@bartoostveen.nl>
2026-08-13 06:42:16 +0200

    matrix/alertmanager: use module from bart-packages

--- modules/infra/matrix/alertmanager.nix
+++ modules/infra/matrix/alertmanager.nix
@@ -2,68 +2,37 @@
   lib,
   pkgs,
   config,
+  inputs,
   ...
 }:
 
 let
-  inherit (lib) getExe mkIf;
+  inherit (lib) mkIf;
 in
 {
+  imports = [ inputs.bart-packages.nixosModules.alertmanager-matrix ];
+
   config = mkIf (config.infra.matrix.enable && config.infra.matrix.alertmanager.enable) {
+    services.alertmanager-matrix = {
+      enable = true;
+      package = pkgs.local.alertmanager-matrix;
+      useLocalAlertmanager = true;
+      environmentFile = config.sops.secrets.alertmanager-matrix-env.path;
+      settings = {
+        message.type = "m.text";
+        log.level = "debug";
+        show.labels = true;
+        homeserver = "https://${config.infra.matrix.domain}";
+        user.id = "@alerts:bartoostveen.nl";
+      };
+    };
+
     systemd.services.alertmanager-matrix = {
-      description = "Alertmanager Matrix bot";
       after = [
-        "network.target"
         "continuwuity.service"
         "sops-install-secrets.service"
       ];
       requires = [ "sops-install-secrets.service" ];
-      wantedBy = [ "multi-user.target" ];
-      environment =
-        let
-          alertmanager = config.services.prometheus.alertmanager;
-        in
-        {
-          ARGS = "";
-          MESSAGE_TYPE = "m.text";
-          LOG_LEVEL = "debug";
-          SHOW_LABELS = "true";
-          HOMESERVER = "https://${config.infra.matrix.domain}";
-          USER_ID = "@alerts:bartoostveen.nl";
-          ALERTMANAGER = "http://${alertmanager.listenAddress}:${toString alertmanager.port}";
-        };
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = getExe pkgs.local.alertmanager-matrix;
-        EnvironmentFile = config.sops.secrets.alertmanager-matrix-env.path;
-        Restart = "on-failure";
-        DynamicUser = true;
-        AmbientCapabilities = "";
-        CapabilityBoundingSet = "";
-        LockPersonality = true;
-        MountAPIVFS = true;
-        NoNewPrivileges = true;
-        PrivateDevices = true;
-        PrivateMounts = true;
-        PrivateUsers = true;
-        PrivateTmp = true;
-        ProtectClock = true;
-        ProtectControlGroups = "strict";
-        ProtectHome = true;
-        ProtectHostname = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        ProtectProc = "invisible";
-        ProtectSystem = "strict";
-        RemoveIPC = true;
-        RestrictAddressFamilies = "AF_INET AF_INET6 AF_UNIX";
-        RestrictNamespaces = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-        SystemCallArchitectures = "native";
-        UMask = 27;
-      };
     };
 
     sops.secrets.alertmanager-matrix-env = {
