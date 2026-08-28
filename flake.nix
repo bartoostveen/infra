7ad9d0fe95827b12e82a5a6c7422ff1ee1bad4ed
Bart Oostveen <bart@bartoostveen.nl>
2026-08-28 17:22:55 +0200

    matrix-continuwuity: 26.8.1 -> 26.8.1-unstable-2026-08-28

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     nixos-small.url = "github:nixos/nixpkgs/nixos-unstable-small"; # Generally more up-to-date kernel
     # nixpkgs-continuwuity.follows = "nixos-small"; # .url = "github:bartoostveen/nixpkgs/matrix-continuwuity-26.7.0";
     # TODO: remove
-    nixpkgs-continuwuity.url = "github:Henry-Hiles/nixpkgs/bump-c10y";
+    nixpkgs-continuwuity.url = "github:bartoostveen/nixpkgs/matrix-continuwuity-26.8.1-unstable-2026-08-28";
 
     ip-bans = {
       url = "git+ssh://forgejo@git.bartoostveen.nl/bart/ip-bans.git";
