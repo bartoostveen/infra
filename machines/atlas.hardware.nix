87501b84d7d24bb628a6a3e8db8620f8c5e3e7cb
Bart Oostveen <bart@bartoostveen.nl>
2026-09-16 15:14:22 +0200

    atlas: do not override kernel anymore

--- machines/atlas.hardware.nix
+++ machines/atlas.hardware.nix
@@ -1,7 +1,6 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 
 {
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_12;
   boot.kernelParams = [
     "console=ttyS1,115200n8"
     "console=tty0"
